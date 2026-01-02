5ec593363475196f8e9a60ac25c8879de280bd3b
uuboyscy <aegis12321@gmail.com>
2026-01-02 23:45:18 +0800

    feat: :sparkles: Add test deployment

---
+++ src/deployments/test_deployment.py
@@ -0,0 +1,13 @@
+from flows.test_flow import test_flow
+
+if __name__ == "__main__":
+    test_flow.from_source(
+        source="https://github.com/uuboyscy/geekbench_report_automation.git",
+        entrypoint="src/flows/test_flow.py:test_flow",
+    ).deploy(
+        name="test",
+        work_pool_name="process-pool",
+        # image="my-image",
+        # push=False,
+        cron="0 * * * *",
+    )
